99b12beb574ccd7d300f7b66a7b876561accd96b
Ekaterina Volkova <ekaterina.volkova@itechart-group.com>
2019-01-17 17:55:28 +0300

    Add pug engine and several routes

--- app.js
+++ app.js
@@ -1,10 +1,27 @@
 import express from 'express';
 import config from 'config/config';
 
+import news from './news.json';
+
 const app = express();
 
+app.set('view engine', 'pug');
+app.set('views', './views');
+
+app.use(express.static(`${__dirname}/public`));
+
+
 app.get('/', (req, res) => {
-  res.send('Hello World');
+  res.render('home', { pageTitle: 'Home' });
+});
+
+app.get('/news', (req, res) => {
+  res.render('news', { news, pageTitle: 'News' });
+});
+
+app.get('/news/:id', (req, res) => {
+  const article = news.find(item => item.id === req.params.id);
+  res.render('article', { pageTitle: article.title, article });
 });
 
 
