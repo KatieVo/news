fa76adc42830a8749fb90253f538280f224d4158
Ekaterina Volkova <ekaterina.volkova@itechart-group.com>
2019-01-24 17:57:51 +0300

    Add sequilize

--- utils/database.js
+++ utils/database.js
@@ -1,10 +1,20 @@
-import mysql from 'mysql2';
+import { Sequelize } from 'sequelize';
 
-const db = mysql.createPool({
+const sequelize = new Sequelize('news_webpage', 'root', 'Qazxdews123', {
+  dialect: 'mysql',
   host: 'localhost',
-  user: 'root',
-  password: 'Qazxdews123',
-  database: 'news_webpage',
 });
 
-export default db.promise();
+export default sequelize;
+
+// FIrst realisation without ORM
+// import mysql from 'mysql2';
+
+// const db = mysql.createPool({
+//   host: 'localhost',
+//   user: 'root',
+//   password: 'Qazxdews123',
+//   database: 'news_webpage',
+// });
+
+// export default db.promise();
